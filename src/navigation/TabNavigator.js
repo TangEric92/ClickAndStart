42dc301cbbaed2c787efad78973b11da54b16879
Eric <you@example.com>
2020-02-29 13:23:41 +0100

    Add icon on tab navigation

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -9,12 +9,32 @@ import Plus from "../screens/Plus";
 
 //--- Tabnavigator
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "search" : "search";
+          } else if (route.name === "Carte") {
+            iconName = focused ? "map-marker" : "map-marker";
+          } else if (route.name === "Clients") {
+            iconName = focused ? "user" : "user";
+          } else if (route.name === "Favoris") {
+            iconName = focused ? "heart" : "heart";
+          } else if (route.name === "Plus") {
+            iconName = focused ? "th-list" : "th-list";
+          }
+          return <FontAwesome name={iconName} size={26} color={color} />;
+        }
+      })}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Carte" component={Carte} />
       <Tab.Screen name="Clients" component={Clients} />
